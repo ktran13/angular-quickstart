41050a8fce8934c73b98e0f83e95d2c422f29227
ktran13 <kristiantran@gmail.com>
2016-10-19 23:20:54 -0400

    adding a search component to the app!

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { RouterModule }   from '@angular/router';
 import { HttpModule }    from '@angular/http';
+import './rxjs-extensions';
 
 // Imports for loading & configuring the in-memory web api
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api/in-memory-web-api.module';
@@ -12,6 +13,7 @@ import { AppComponent }   from './app.component';
 import { DashboardComponent }     from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
+import { HeroSearchComponent }     from './hero-search.component';
 import { HeroService }         from './hero.service';
 
 @NgModule({
@@ -44,7 +46,8 @@ import { HeroService }         from './hero.service';
     AppComponent,
     DashboardComponent,
     HeroDetailComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroSearchComponent
   ],
   providers: [
     HeroService
